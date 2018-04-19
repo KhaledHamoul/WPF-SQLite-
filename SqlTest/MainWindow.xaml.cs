49ed2b3b740584136a5c914bec572221f295b6ed
KhaledHamoul <fk_hamoul@esi.dz>
2018-04-19 16:32:35 +0100

    first commit

---
+++ SqlTest/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using System.Windows;
+using System.Windows.Data;
+
+
+namespace SqlTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Database db = new Database();
+            var id = "'khaled'"; 
+            db.commande.CommandText = "SELECT * FROM users WHERE users.username = " + id  ;
+            db.dataReader = db.commande.ExecuteReader();
+
+           
+
+            while (db.dataReader.Read())
+            {
+                Console.WriteLine("==================================================================");
+
+                Console.WriteLine("id : " + db.dataReader.GetInt32(0) + "nom : " + db.dataReader.GetString(1) + "MDP : " + db.dataReader.GetString(2));
+                Console.WriteLine("==================================================================");
+
+
+            }
+
+            db.commande.Dispose();
+            var username = "salah ";
+            var password = "psd";
+            string cmd = "INSERT INTO users (username , password ) VALUES ('" + username + "','"  + password + "');";
+
+            db.commande.CommandText = cmd;
+
+
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+            Console.WriteLine(cmd);
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            db.commande.ExecuteNonQuery();
+
+
+
+
+
+        }
+    }
+}
---
+++ SqlTest/database/Database.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SqlTest
+{
+    public class Database
+    {
+
+        private SQLiteConnection connection;
+        public SQLiteCommand commande;
+        public SQLiteDataReader dataReader;
+
+        public Database()
+        {
+            connection = new SQLiteConnection("Data Source=bezzierDB.db;New=true;Compress=true;");
+            connection.Open();
+            commande = connection.CreateCommand();
+        }
+       
+
+
+    }
+}
